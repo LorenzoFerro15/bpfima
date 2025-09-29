ee66e88b001150d248b21f74b55673478b5f4c37
lorenzoferro15@gmail.com <lorenzoferro15@gmail.com>
2025-09-29 14:51:03 +0200

    minor fixes

--- kfunc_tpm.c
+++ kfunc_tpm.c
@@ -16,6 +16,87 @@ extern int bpf_tpm_extend_pcr(const char *data, u32 data_len) __ksym;
 extern int bpf_ima_extend_measurement(const char *event_name, const char *data, u32 data_len) __ksym;
 extern int bpf_ima_get_pcr_value(char *pcr_buf, u32 buf_size) __ksym;
 
+/* Helper function to convert u32 to string and append to buffer */
+static __always_inline int append_u32_to_buffer(char *buf, int *len, int max_len, u32 value)
+{
+    char temp[16];
+    int temp_len = 0;
+    
+    /* Handle zero case */
+    if (value == 0) {
+        if (*len >= max_len - 1) return -1;
+        buf[(*len)++] = '0';
+        return 0;
+    }
+    
+    /* Convert to string (digits in reverse order) */
+    u32 temp_val = value;
+    while (temp_val > 0 && temp_len < 15) {
+        temp[temp_len++] = '0' + (temp_val % 10);
+        temp_val /= 10;
+    }
+    
+    /* Check if we have space */
+    if (*len + temp_len >= max_len) return -1;
+    
+    /* Reverse and copy to buffer */
+    for (int i = temp_len - 1; i >= 0; i--) {
+        buf[(*len)++] = temp[i];
+    }
+    
+    return 0;
+}
+
+/* Helper function to append string to buffer */
+static __always_inline int append_string_to_buffer(char *buf, int *len, int max_len, const char *str, int str_max_len)
+{
+    for (int i = 0; i < str_max_len && str[i] != 0; i++) {
+        if (*len >= max_len - 1) return -1;
+        buf[(*len)++] = str[i];
+    }
+    return 0;
+}
+
+/* Helper function to append separator to buffer */
+static __always_inline int append_separator(char *buf, int *len, int max_len)
+{
+    if (*len >= max_len - 1) return -1;
+    buf[(*len)++] = '_';
+    return 0;
+}
+
+/* Build measurement data string: "comm_pid_uid" */
+static __always_inline int build_measurement_data(char *measurement_data, int max_len, 
+                                                  const char *comm, pid_t pid, u32 uid)
+{
+    int len = 0;
+    
+    /* Add process name */
+    if (append_string_to_buffer(measurement_data, &len, max_len, comm, 16) < 0)
+        return -1;
+    
+    /* Add separator and PID */
+    if (append_separator(measurement_data, &len, max_len) < 0)
+        return -1;
+    if (append_u32_to_buffer(measurement_data, &len, max_len, pid) < 0)
+        return -1;
+    
+    /* Add separator and UID */
+    if (append_separator(measurement_data, &len, max_len) < 0)
+        return -1;
+    if (append_u32_to_buffer(measurement_data, &len, max_len, uid) < 0)
+        return -1;
+    
+    /* Null terminate */
+    if (len < max_len) {
+        measurement_data[len] = '\0';
+    } else {
+        return -1;
+    }
+    
+    return len;
+}
+
 /* Monitor file unlink operations with TPM integration */
 SEC("tracepoint/syscalls/sys_enter_unlinkat")
 int handle_unlinkat_tpm(void *ctx)
@@ -25,6 +106,7 @@ int handle_unlinkat_tpm(void *ctx)
     u32 uid = uid_gid & 0xFFFFFFFF;
     u32 gid = uid_gid >> 32;
     char comm[16];
+    char pcr_buf[128];
     u64 ts = bpf_ktime_get_ns();
     
     bpf_get_current_comm(comm, sizeof(comm));
@@ -37,91 +119,28 @@ int handle_unlinkat_tpm(void *ctx)
     int tpm_available = bpf_tpm_is_available();
     bpf_printk("TPM Available: %s\n", tpm_available ? "YES" : "NO");
     
-    /* Build measurement data */
+    /* Build measurement data using helper function */
     char measurement_data[256] = {0};
-    char *ptr = measurement_data;
-    int len = 0;
+    int data_len = build_measurement_data(measurement_data, sizeof(measurement_data), comm, pid, uid);
     
-    /* Add process information to measurement */
-    for (int i = 0; i < 16 && comm[i] != 0 && len < 200; i++) {
-        *ptr++ = comm[i];
-        len++;
+    if (data_len < 0) {
+        bpf_printk("Failed to build measurement data\n");
+        return 0;
     }
     
-    if (len < 250) {
-        *ptr++ = '_';
-        len++;
-        
-        /* Add PID */
-        u32 temp_pid = pid;
-        char pid_str[16];
-        int pid_len = 0;
-        
-        if (temp_pid == 0) {
-            pid_str[pid_len++] = '0';
-        } else {
-            char temp[16];
-            int temp_len = 0;
-            while (temp_pid > 0 && temp_len < 15) {
-                temp[temp_len++] = '0' + (temp_pid % 10);
-                temp_pid /= 10;
-            }
-            for (int i = temp_len - 1; i >= 0 && pid_len < 15; i--) {
-                pid_str[pid_len++] = temp[i];
-            }
-        }
-        
-        for (int i = 0; i < pid_len && len < 245; i++) {
-            *ptr++ = pid_str[i];
-            len++;
-        }
-        
-        /* Add UID */
-        if (len < 240) {
-            *ptr++ = '_';
-            len++;
-            
-            u32 temp_uid = uid;
-            char uid_str[16];
-            int uid_len = 0;
-            
-            if (temp_uid == 0) {
-                uid_str[uid_len++] = '0';
-            } else {
-                char temp[16];
-                int temp_len = 0;
-                while (temp_uid > 0 && temp_len < 15) {
-                    temp[temp_len++] = '0' + (temp_uid % 10);
-                    temp_uid /= 10;
-                }
-                for (int i = temp_len - 1; i >= 0 && uid_len < 15; i--) {
-                    uid_str[uid_len++] = temp[i];
-                }
-            }
-            
-            for (int i = 0; i < uid_len && len < 235; i++) {
-                *ptr++ = uid_str[i];
-                len++;
-            }
-        }
-    }
-    
-    *ptr = '\0';
-    
-    bpf_printk("Measurement data: %s (len=%d)\n", measurement_data, len);
+    bpf_printk("Measurement data: %s (len=%d)\n", measurement_data, data_len);
     
     /* Extend IMA measurement list */
     char event_name[] = "file_unlink";
-    int ima_ret = bpf_ima_extend_measurement(event_name, measurement_data, len);
+    int ima_ret = bpf_ima_extend_measurement(event_name, measurement_data, data_len);
     bpf_printk("IMA measurement result: %d\n", ima_ret);
     
     /* TPM operations if available */
     if (tpm_available) {
-        int tpm_ret = bpf_tpm_extend_pcr(measurement_data, len);
+        int tpm_ret = bpf_tpm_extend_pcr(measurement_data, data_len);
         bpf_printk("TPM PCR extend result: %d\n", tpm_ret);
         
         /* Get updated PCR value */
-        char pcr_buf[128];
         int pcr_ret = bpf_ima_get_pcr_value(pcr_buf, sizeof(pcr_buf));
         if (pcr_ret == 0) {
             bpf_printk("PCR Value: %s\n", pcr_buf);
@@ -131,8 +150,6 @@ int handle_unlinkat_tpm(void *ctx)
     } else {
         bpf_printk("TPM not available - using simulation only\n");
         
-        /* Still get simulated PCR value */
-        char pcr_buf[128];
         int pcr_ret = bpf_ima_get_pcr_value(pcr_buf, sizeof(pcr_buf));
         if (pcr_ret == 0) {
             bpf_printk("Simulated PCR: %s\n", pcr_buf);
